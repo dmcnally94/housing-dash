1438eee227f9cab3bfb928bd53d3f01eb3fe6ca7
Ryan McNally <ryan@vidora.com>
2020-04-26 18:50:19 -0700

    Fix structure of project

---
+++ database/transforms.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import sqlite3
+import dash
+from dash.dependencies import Input, Output
+import dash_table
+import pandas as pd
+
+path = f"{Path(__file__).resolve().parent.parent}/data/wine_data.sqlite"
+conn = sqlite3.connect(path)
+c = conn.cursor()
+
+df = pd.read_sql("select * from wine_data", conn)
+
+df = df[['country','description','rating','price','province','title','variety','winery','color','varietyID']]
