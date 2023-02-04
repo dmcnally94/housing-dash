325c4648cba2f69eed7fbf9c7c1311cc2729e4dc
Devin McNally <mcnally.devinjohn@gmail.com>
2023-02-04 15:41:11 -0500

    v02042023c

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import dash_table
+from dash import dash_table
 import pandas as pd
 import plotly.express as px
 import plotly.figure_factory as ff
